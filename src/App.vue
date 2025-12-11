905ca63e09c6a847a9b7df791cc339bc61cb514c
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-12-12 10:35:57 +1100

    batman

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const foo = ref<HTMLDivElement|null>(null)
+</script>
+
+<template>
+  <div ref="foo"></div>
+</template>
+
+<style scoped></style>
